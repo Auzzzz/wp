686ae2f564d061015f7300ea6c3f11cde87853a8
Datkelly423 <ck200.423@gmx.com>
2018-08-27 14:29:22 +1000

    Flex  Update moved over to flex

--- a2/index.php
+++ a2/index.php
@@ -7,40 +7,44 @@
     <!-- Keep wireframe.css for debugging, add your css to style.css -->
     <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
     <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Merienda" rel="stylesheet">
+
+    <!-- JS -->
     <script src='../wireframe.js'></script>
   </head>
 
   <body>
 
     <header>
-      <div class="logo">
+      <div class="logo"></div>
+      <div class ="smlogo"></div>
     </header>
 
-    <nav>
-      <ul>
-        <li>
-           <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">Test </a>
-        </li>
-        <li>
-          <a href="#"> Test</a>
-        </li>
-        <li>
-          <a href="#">Test</a>
-        </li>
-        <li>
-          <a href="#">Test</a>
-        </li>
-      </ul>
+    <nav id="home">
+      <div class='menu'>
+        <div class='nav'> Home</div>
+        <div class='nav'>Login</div>
+        <div class='nav'>Products</div>
+        <div class="nav">Special</div>
+      </div>
     </nav>
 
-    <main>
-      <article id='Website Under Construction'>
-    <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
-        <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
-      </article>
+    <main id="home">
+      <section class="info-grid">
+        <div class="box-grid">
+          <div class="grid-display">
+            <h1> Why us? </h1>
+            <h3> Fortune Hosting is the best all round server hosting provider within Australia. With 15 years’ experience the hosting world along with over 500 happy customers you know you’re in good hands. Along with our top of the range Pentium © servers we can host anything and everything that you require from a simple email server to a mega file system </h3>
+          </div>
+          <div class="grid-display">
+            <h3> test </h3>
+          </div>
+          <div class="grid-display">
+            <h3> test </h3>
+          </div>
+        </div>
     </main>
 
     <footer>
